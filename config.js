460f7e5760d3f9d59e76218c51a1b55369f88d04
LeKhanhDuy <132117429+LeKhanhDuy2010@users.noreply.github.com>
2023-12-12 20:29:30 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Thúy Duy';
+const giftUrl = 'https://web.facebook.com/nguyenthuy.duy.7583/map';
+const eventName = 'Happy BirthDay Lần 21';
+const titleCard = 'Tặng ní';
+const contentCard = 'Chúc ní có một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với ní không chỉ trong hôm nay mà còn cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
